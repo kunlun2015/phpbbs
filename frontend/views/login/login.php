0a3a65f37cf506723f6f8354c23750f0d6ea7d4f
amos <szhcool1129@sina.com>
2017-08-22 17:38:31 +0800

    login page

--- frontend/views/login/login.php
+++ frontend/views/login/login.php
@@ -5,6 +5,43 @@
  * @date: 2017-08-16 18:34
  * @version $Id$
  */
-
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+    \frontend\assets\AppAsset::register($this);
+    \frontend\assets\AppAsset::addCss($this, 'static/css/login.css');
     $this->title = '用户登录';
 ?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+    <header>
+        <div class="header">
+            <div class="logo"></div>
+            <div class="page-func">用户登陆</div>
+        </div>
+    </header>
+    <div class="login-wrap">
+        <div class="wrap">
+            <div class="desc"></div>
+            <div class="login-box">
+                <div class="box-title">账号登陆</div>
+                <form action="#" method="post">
+                    <input type="text" name="username" placeholder="用户名">
+                    <input type="password" name="password" placeholder="登陆密码">
+                </form>
+            </div>
+        </div>
+    </div>
+<?php $this->endBody() ?>
+
+</body>
+</html>
+<?php $this->endPage() ?>
