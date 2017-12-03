1d503fdab8514bf92df0e95c5c083eb002f24b5b
Kunlun <szhcool1129@sina.com>
2017-12-03 21:53:15 +0800

    developing

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -10,7 +10,7 @@ use yii\web\AssetBundle;
 class AppAsset extends AssetBundle{
     public $basePath = '@webroot';
     //public $baseUrl = '@web';
-    public $baseUrl = 'http://static.debugphp.com';
+    public $baseUrl = 'http://local.static.debugphp.com';
     public $css = [
         'css/common.css',
     ];
@@ -21,11 +21,11 @@ class AppAsset extends AssetBundle{
 
     //定义按需加载JS方法，注意加载顺序在最后  
     public static function addScript($view, $jsfile) {  
-        $view->registerJsFile('http://static.debugphp.com/'.$jsfile, [/*AppAsset::className(), */'depends' => 'frontend\assets\AppAsset']);  
+        $view->registerJsFile('http://local.static.debugphp.com/'.$jsfile, [/*AppAsset::className(), */'depends' => 'frontend\assets\AppAsset']);  
     }  
       
    //定义按需加载css方法，注意加载顺序在最后  
     public static function addCss($view, $cssfile) {  
-        $view->registerCssFile('http://static.debugphp.com/'.$cssfile, [/*AppAsset::className(), */'depends' => 'frontend\assets\AppAsset']);  
+        $view->registerCssFile('http://local.static.debugphp.com/'.$cssfile, [/*AppAsset::className(), */'depends' => 'frontend\assets\AppAsset']);  
     }  
 }
--- test.php
+++ test.php
@@ -4,18 +4,9 @@
  * @param array $arr
  * @return array 
  */
-function areaListGroupByLetters($arr = []){
-    $county = $county ? $county : [];
-    $letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];        
-    foreach ($letters as $k => $v) {
-        $charterAreaList = [];
-        foreach ($arr as $k2 => $v2) {
-            if($this->get_first_charter($v2) === $v){
-                array_push($charterAreaList, $v2);
-            }
-        }
-        $county[$v] = $charterAreaList;
-    }
-    $county = array_filter($county);
-    return $county;
-}
+$arr = [
+    1=> ['name' => 'amos'],
+    2=> ['name' => 'kunlun']
+];
+
+print_r(array_values($arr));
